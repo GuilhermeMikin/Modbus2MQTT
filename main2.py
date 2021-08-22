972f349837e6617241f1ceb811b3e3d68acc6eb8
Guilherme Balduino Lopes <guilhermebalopes@ufu.br>
2021-08-21 21:03:14 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -5,8 +5,8 @@ modbus_Addrs = '127.0.0.1'
 modbus_Port = 502
 modbus_DeviceID = 1
 broker_local_Addrs = "127.0.0.1"
-broker_aws_Addrs = "3.134.40.193"
+broker_cloud_Addrs = "****************"
 broker_Port = 1883
 
-c = ClienteMODBUS(modbus_Addrs, modbus_Port, modbus_DeviceID, broker_aws_Addrs, broker_Port, dbPath)
+c = ClienteMODBUS(modbus_Addrs, modbus_Port, modbus_DeviceID, broker_cloud_Addrs, broker_Port, dbPath)
 c.atendimento()
